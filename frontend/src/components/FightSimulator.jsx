98c4f76fb4cd2917b2ca4da3710e5cfb737df37a
JacksonBennett05 <jcb263@gmail.com>
2025-07-13 16:28:15 -0400

    Fight Simulator

--- frontend/src/components/FightSimulator.jsx
+++ frontend/src/components/FightSimulator.jsx
@@ -1,1 +1,33 @@
 // Custom interactive component for UI logic
+
+import { useState } from 'react'
+import { simulateFight } from '../api/api'
+
+function FightSimulator() {
+    const [fighter1, setFighter1] = useState('')
+    const [fighter2, setFighter2] = useState('')
+    const [result, setResult] = useState(null)
+    const [err, setError] = useState('')
+
+
+    const handleSimulate = async () => {
+        try {
+            const res = await simulateFight(fighter1, fighter2);
+            setResult(res)
+            setError('')
+        } catch (err) {
+            setResult(null)
+            setError(err.message || 'An error has occured')
+        }
+    };
+
+    return (
+        <div className="fight-sim">
+            <h1>UFC Fight Simulator</h1>
+            <p>Ready to fight? Click "Fight" button below</p>
+            <button onClick={handleSimulate}>Fight</button>
+        </div>
+    );
+}
+
+export default FightSimulator;
